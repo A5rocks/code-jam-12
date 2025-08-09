62b49f5b232abce847a27caea167a8e9dd4c5106
Daniel <daniel@theheretic.work>
2025-08-08 20:21:40 -0500

    lint: tidy up for checks

--- script.py
+++ script.py
@@ -1,15 +1,15 @@
 import re
 
-
-
 c = re.compile(r"(?i)SELECT (?P<fields>.+) FROM (?P<table>.+)")
 
 
-def do_something(name:str) -> None:
+def do_something(name: str) -> None:
+    """Nothing, just plaiting with functions."""
     print(f"Hello {name}")
 
-def parse_input(sql_data:str):
+
+def parse_input(sql_data: str) -> None:
+    """Start of the parser."""
     data = c.match(sql_data)
     print(data["fields"])
     print(data["table"])
-
