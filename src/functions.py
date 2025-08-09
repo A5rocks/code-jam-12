5f5edfd9fe6d1202883cbdddebd533e3b292371d
Daniel <daniel@theheretic.work>
2025-08-09 08:59:29 -0500

    lint: linting for the PR

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,7 @@
 """The main script file for Pyodide."""
 
 from js import document
+
 # from parser import tokenize
 from pyodide.http import pyfetch
 
@@ -8,7 +9,7 @@ from pyodide.http import pyfetch
 def parse_input(sql_data: str) -> None:
     """Start of the parser."""
     # y =  tokenize(sql_data)
-    # print(y[-2].text) TODO: Put SQL Parser in 
+    # print(y[-2].text) TODO: Put SQL Parser in
     msg = "A5 SQL Parser Needed"
     raise NotImplementedError(msg)
 
@@ -25,14 +26,13 @@ async def get_user_data(user: dict) -> dict:
         data_point = i["post"]
         try:
             tb.innerHTML += f"""<tr>
-            <td colspan="2" style="text-align: center; padding: 20px; color: #666">
-                        {data_point["author"]["displayName"]}
-            </td>
-            <td colspan="4" style="text-align: left; overflow-wrap: anywhere; word-wrap: break-word; padding: 20px; color: #666">
-                        {data_point["record"]["text"].replace("\n","<br>")}
-            </td>
-            </tr>"""
+<td colspan="2" style="text-align: center; padding: 20px; color: #666">
+            {data_point["author"]["displayName"]}
+</td>
+<td colspan="4" style="text-align: left; overflow-wrap: anywhere; word-wrap: break-word; padding: 20px; color: #666">
+            {data_point["record"]["text"].replace("\n", "<br>")}
+</td>
+</tr>"""
         except KeyError:
             continue  # Handle if a field is missing (no text maybe?)
     return val
-
--- src/setup.py
+++ src/setup.py
@@ -5,11 +5,10 @@ from pyodide.http import pyfetch
 
 async def setup_pyodide_scripts() -> None:
     """Script to do everything for pyodide."""
-    response = await pyfetch("./script.py")
-    with open("script.py", "wb") as f:
+    response = await pyfetch("./functions.py")
+    with open("functions.py", "wb") as f:
         f.write(await response.bytes())
 
-    response = await pyfetch("./parser.py")
-    with open("parser.py", "wb") as f:
-        f.write(await response.bytes())
-
+    # response = await pyfetch("./parser.py")
+    # with open("parser.py", "wb") as f:
+    #     f.write(await response.bytes())
