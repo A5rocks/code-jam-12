c899edf1fee56a1b72aaacae1ab482e12f3e4317
Daniel <daniel@theheretic.work>
2025-08-08 20:18:55 -0500

    feat: added a button for onclick and a text input

--- script.py
+++ script.py
@@ -1,4 +1,15 @@
+import re
+
+
+
+c = re.compile(r"(?i)SELECT (?P<fields>.+) FROM (?P<table>.+)")
 
 
 def do_something(name:str) -> None:
     print(f"Hello {name}")
+
+def parse_input(sql_data:str):
+    data = c.match(sql_data)
+    print(data["fields"])
+    print(data["table"])
+
